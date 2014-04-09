c25b8c0e15b811cd31b326b125b66eade826a611
viveknabhi <viveknabhi@gmail.com>
2014-04-09 09:53:24 -0400

    Created UI Folder Structure

---
+++ frontend/server.js
@@ -0,0 +1,24 @@
+// server.js (final)
+
+	// set up ======================================================================
+	var express  = require('express');
+	var app      = express(); 								// create our app w/ express
+	var port  	 = process.env.PORT || 8080; 				// set the port
+	//var database = require('./app/config/database'); 		// load the database config
+
+	// configuration ===============================================================
+	//Connect to Neo4j here
+
+	app.configure(function() {
+		app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+		app.use(express.logger('dev')); 						// log every request to the console
+		app.use(express.bodyParser()); 							// pull information from html in POST
+		app.use(express.methodOverride()); 						// simulate DELETE and PUT
+	});
+
+	// routes ======================================================================
+	//require('./app/routes.js')(app);
+
+	// listen (start app with node server.js) ======================================
+	app.listen(port);
+	console.log("App listening on port " + port);
