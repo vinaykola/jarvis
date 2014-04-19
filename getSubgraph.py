880d537e725c326adeff4a438ffb629e0e14616a
amish-goyal <amish1804@gmail.com>
2014-04-19 07:58:21 -0400

    Added getAllNodesofDepth function

--- getSubgraph.py
+++ getSubgraph.py
@@ -64,4 +64,44 @@ def getSubgraph():
     print op
     json.dump(op,open('subgraph.json','w'))
 
-getSubgraph()
+def getAllNodesOfDepth(name,num):
+    op={"nodes":[],"links":[]}
+    nodes=[]
+
+    db1 = GraphDatabase("http://localhost:7474/db/data/")
+    q = '''MATCH (n { name: \''''+name+'''\'})-[*'''+str(num)+''']->m RETURN n,r,m'''
+    results = db1.query(q,returns=(client.Node, client.Relationship, client.Node))#, unicode, client.Relationship))
+
+    for result in results:
+        n1=result[0].properties['name']
+        n2=result[2].properties['name']
+        try:
+            i1=nodes.index(n1)
+        except:
+            nodes.append(n1)
+            i1=nodes.index(n1)
+            op["nodes"].append({"name":n1})
+        try:
+            i2=nodes.index(n2)
+        except:
+            nodes.append(n2)
+            i2=nodes.index(n2)
+            op["nodes"].append({"name":n2})
+        
+        r = result[1].type
+        op["links"].append({"source":i1,"target":i2,"type":r})
+    json.dump(op,open('subgraph.json','w'))
+    return op
+    """
+    endnode = []
+    for i in xrange(len(results)):
+        for word in results[i]:
+            endnode.append(str(word.properties['name']))
+
+    for i in xrange(len(endnode)):
+        print endnode[i]
+    return endnode
+    """
+
+#getSubgraph()
+getAllNodesOfDepth("batman",2)
