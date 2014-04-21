b67d8eab0fda04749d198570313b95c043ca2d2a
codenut27 <krishnan.vinodh@gmail.com>
2014-04-20 20:52:40 -0400

    Take it and go

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from util import util
 from characterAPI import characterAPI
 
 character = characterAPI("Superman")
-character.setupFeatures()
+character.setUpFeatures("http://www.comicvine.com/api/characters?api_key=37a0f6cdbe5752b2f272373ba6a21491ea2629eb&filter=name:Superman&format=json")
 aliases = character.get_aliases()
 birth = character.get_birth()
 powers = character.get_powers()
